44f4d2d189dcfff9ce206240a7078e4284638934
Chris Nilghe <chris@nilghe.com>
2012-12-22 15:55:05 -0600

    Animation, CSS, JS, & Card Updates
    - Updated the animation timing to flow better
    - Updated the CSS for more polish
      - Text Shadow
      - Better centering of objects
    - Added last names in some of the cards
    - Simplified the js to only read cards.json once and check the inputted
    code against the actual cards and not a hardcoded array.

--- js/main.js
+++ js/main.js
@@ -12,85 +12,66 @@ $(document).ready(function() {
 /* Check the code the user entered to see if they have a card */
 function checkCode(code) {
 
-    var array = [	"nilghe4", 
-					"himpster2", 
-					"anna8", 
-					"scheirer88",
-					"wdwmarriage",
-					"cjme980",
-					"cutebutton",
-					"brewer",
-					"applebois",
-					"twitters",
-					"sneakysneaky",
-					"kate",
-					"bbqmaster",
-					"utsunomiya",
-					"cousin",
-					"parham",
-					"amazingrace",
-					"ember",
-					"toofarr",
-					"mandee",
-                    "merrychristmas"];
+	var hasCard = false;
 
-    if (jQuery.inArray(code, array) != -1)
-    {
-	
-		$('#instruction-wrapper').css('z-index', '2');
-		$('#loader').fadeIn(2000);
-		
-		fillCard(code);
-		
-		//Shift background image
-		$('.redbox').animate({"backgroundPosition" : "-1000px"}, 20000);
-		
-		//Shift the instructions off screen
-		// $('#instruction').animate({"left":"-600px"},2000, function(){
-			// showLoading("#loadingOne", 1);
-			// window.setTimeout(showLoading, 4000, "#loadingTwo", 1);
-			// window.setTimeout(showLoading, 8000, "#loadingThree", 0);
-		// });
-		
-		$('#instruction').animate({"left":"-600px"},2000);
-		$('#loadingOne').queue(function() {
-			showLoading($(this), 1);
-			$(this).dequeue();
-		});
-		$('#loadingTwo').delay(4000).queue(function() {
-			showLoading($(this), 1);
-			$(this).dequeue();
-		});
-		$('#loadingThree').delay(8000).queue(function() {
-			showLoading($(this), 0);
-			$(this).dequeue();
-		});
-    }
-    else
-    {
-        alert("Sorry there is no card for you");
-    }
-}
-
-/* Grab the card from json and display */
-function fillCard(code) {
 	$.getJSON("cards.json",
-    function(data) {
-        $.each(data.cards, function(i, item) {
+    function(cardList) {
+        $.each(cardList.cards, function(i, item) {
 			if (item.id == code){
-				$('#name').append(item.name);
-				$('#message').append("<p>" + item.message + "</p>");
+				hasCard = true;
+				return;
 			}
         });
 
+	    if (hasCard)
+	    {
+		
+			$('#instruction-wrapper').css('z-index', '2');
+			$('#loader').fadeIn(2000);
+			
+			fillCard(code, cardList);
+			
+			//Shift background image
+			$('.redbox').animate({"backgroundPosition" : "-1000px"}, 20000);
+			
+			$('#instruction').animate({"left":"-9999px"}, 5000);
+			$('#loadingOne').queue(function() {
+				showLoading($(this), 1);
+				$(this).dequeue();
+			});
+			$('#loadingTwo').delay(4000).queue(function() {
+				showLoading($(this), 1);
+				$(this).dequeue();
+			});
+			$('#loadingThree').delay(8000).queue(function() {
+				showLoading($(this), 0);
+				$(this).dequeue();
+			});
+	    }
+	    else
+	    {
+	        alert("Sorry there is no card for you");
+	    }
+
+    });
+
+}
+
+/* Grab the card from json and display */
+function fillCard(code, cardList) {
+    $.each(cardList.cards, function(i, item) {
+		if (item.id == code){
+			$('#name').append(item.name);
+			$('#message').append("<p>" + item.message + "</p>");
+		}
     });
 }
 
 //Show the loading Text and animations
 function showLoading(id, finish) {
 
-	$(id).animate({"left":"50%"}, 2000, function(){
-		$(id).delay("1000").animate({left:"-600px"}, 2000, function(){
+	$(id).animate({"left":"50%"}, 5000, function(){
+		$(id).delay("1000").animate({left:"-9999px"}, 5000, function(){
 			if (finish == 0) { window.setTimeout(showCard, 1000); } //Show card once we are done "loading"
 		});
 	});
@@ -102,17 +83,19 @@ function showCard() {
 	
 	$('#loader').fadeOut(2000);
 	
-	//Shift Reciepant on screen
-	$('#name').animate({"left":"4%"},3000, function(){
-		//Shift Vertical Ribbon
-		$('#redboxVertical').animate({"left":"80px"}, 2000);
+	
+	//Shift Vertical Ribbon
+	$('#redboxVertical').animate({"left":"80px"}, 2000, function(){
+
+		//Shift Reciepant on screen
+		$('#name').animate({"left":"4%"}, 4000);
 
 		//Reset the z-index of the instruction wrapper so the card displays properly
 		$('#instruction-wrapper').css('z-index', '');
 
 		//Display Card
 		$('#card-wrapper').show();
-		$('#card-wrapper').animate({"height":"400px"}, 2000, function(){
+		$('#card-wrapper').animate({"height":"500px"}, 2000, function(){
 			$('#message').fadeIn('slow');
 		});
 	});
